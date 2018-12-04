604b46228192a00cc8f823262bddd20771e57a5e
pphos <coc4h8conhc6h12nh@gmail.com>
2018-12-04 21:47:31 +0900

    remove resize_image function

--- src/modules/utils/preprocess.py
+++ src/modules/utils/preprocess.py
@@ -31,28 +31,3 @@ def adjust_to_keras_input_image(X, y, nb_classes):
     y = to_categorical(y, nb_classes)
 
     return X, y
-
-
-def resize_images(img_path, img_shape):
-    """
-    画像のリサイズ
-    # Arguments
-    img_path    : 画像のパス
-    img_shape   : リサイズする画像のサイズのタプル
-                  (ex. img_shape = (img_x, img_y))
-
-    # Returns:
-    img_resized : リサイズ後の画像のNumpy配列
-    """
-    import cv2
-
-    image = cv2.imread(img_path)
-    img_resized = cv2.resize(image, img_shape)
-
-    return img_resized
-
-
-if __name__ == '__main__':
-    img_path = '../../../tiny_datasets/内田真礼/30263767.jpeg'
-
-    print(resize_images(img_path, (256, 256)))
