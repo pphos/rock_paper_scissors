0e030d5332b2fd627bc32f884071b9eb90c4cacb
pphos <coc4h8conhc6h12nh@gmail.com>
2018-12-04 21:48:37 +0900

    add convert_imgs_to_arrays func

---
+++ src/preprocess/convert_imgs_to_arrays.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+import os
+import pickle
+import argparse
+import numpy as np
+import cv2
+
+TRINING_FEATURE_NAME = 'training_features'
+EVALUATION_FEATURE_NAME = 'eval_features'
+GRAY_SCALE = 1
+
+
+def convert_imgs_to_arrays(input_dir, dataset_type, save_dir, img_shape):
+    """
+    データセットの画像をNumpy配列に変換
+    # Arguments
+        input_dir   : 入力画像があるフォルダへのパス
+        save_dir    : Numpy形式の画像を保存するフォルダへのパス
+    """
+    # 保存ファイル名の設定
+    data_path = os.path.join(save_dir, dataset_type, 'data.npy')
+    target_path = os.path.join(save_dir, dataset_type, 'target.npy')
+    target_label_path = os.path.join(save_dir, dataset_type,
+                                     'target_label.pkl')
+    # カテゴリの取得
+    categories = os.listdir(input_dir)
+    # データを保存するNumpy配列の初期化
+    data, target = __init_np_array(input_dir, categories, img_shape)
+
+    progress = 0
+    for index, category in enumerate(categories):
+        category_path = os.path.join(input_dir, category)
+        for image_name in os.listdir(category_path):
+            image_path = os.path.join(category_path, image_name)
+
+            # 画像のリサイズ
+            image = resize_image(image_path, img_shape)
+            # Numpy配列に訓練・教師データの格納
+            data[progress] = image
+            target[progress] = index
+
+            progress += 1
+            print("{:04d}: {} {} :{}".format(progress, index,
+                                             category, image_name))
+
+    # 訓練・教師データおよびラベルの保存
+    with open(target_label_path, 'wb') as f:
+        pickle.dump(categories, f)
+    np.save(data_path, data[:progress])
+    np.save(target_path, target[:progress])
+
+
+def resize_image(img_path, img_shape):
+    """
+    画像のリサイズ
+    # Arguments
+        img_path    : 画像のパス
+        img_shape   : リサイズする画像のタプル
+    # Returns
+        img_resized : リサイズ後の画像のNumpy配列
+    """
+    image = cv2.imread(img_path)
+    img_resized = cv2.resize(image, img_shape, img_shape)
+
+    return img_resized
+
+
+def __init_np_array(input_dir, categories, img_shape):
+    """
+    データを保存するNumpy配列の初期化
+    # Arguments:
+        input_dir   : 入力画像があるフォルダへのパス
+        categories  : 画像のカテゴリを格納したリスト
+        img_shape   : リサイズする画像サイズ
+    # Returns:
+        data        : 全要素が0の訓練データ用のNumpy配列
+        target      : 全要素が0の教師データ用のNumpy配列
+    """
+    # データセット内の総ファイル数の算出
+    total_file_nums = __calc_total_file_nums(input_dir, categories)
+    target = np.zeros((total_file_nums,), dtype='int32')
+
+    # 1ファイル分の画像を読み込み,
+    # total_file_numsを元に必要なNumpy配列のサイズを算出
+    data = None
+    for root, dirs, files in os.walk(input_dir):
+        for fname in files:
+            # 1ファイル分の画像のリサイズ
+            img_path = os.path.join(root, fname)
+            img = resize_image(img_path, img_shape)
+
+            if len(img.shape) == GRAY_SCALE:
+                data = np.zeros((total_file_nums, *img_shape))
+            else:
+                data = np.zeros((total_file_nums, *img_shape, img.shape[-1]))
+            break
+
+        if data is not None:
+            break
+
+    return data, target
+
+
+def __calc_total_file_nums(input_dir, categories):
+    """
+    データセット内の総ファイル数の算出
+    # Arguments:
+        input_dir       : 入力画像があるフォルダへのパス
+        categories      : 画像のカテゴリを格納したリスト
+    # Returnes
+        total_file_nums : データセット内の総ファイル数
+    """
+    total_file_nums = 0
+    for category in categories:
+        category_path = os.path.join(input_dir, category)
+        total_file_nums += len(os.listdir(category_path))
+
+    return total_file_nums
+
+
+if __name__ == '__main__':
+    input_dir = '../../datasets/training_set'
+    save_dir = '../../datasets'
+    img_shape = 256
+
+    convert_imgs_to_arrays(
+        input_dir, TRINING_FEATURE_NAME, save_dir, img_shape)
