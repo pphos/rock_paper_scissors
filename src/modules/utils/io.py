23d6677001668b7c7518c09d5babe23088525ee4
pphos <coc4h8conhc6h12nh@gmail.com>
2018-12-05 14:45:03 +0900

    add load_dataset func

---
+++ src/modules/utils/io.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import pickle
+import numpy as np
+
+from . import Bunch
+
+
+def load_binary_file(filepath):
+    """
+    pickle形式のバイナリデータの読み込み
+    # Arguments:
+        filepath    : 読み込む対象ファイル (pickle形式)
+    # Returns
+        data        : 読み込んだpickleデータ
+    """
+    with open(filepath, 'rb') as f:
+        data = pickle.load(f)
+    return data
+
+
+def load_dataset(data_path, target_path, target_label_path):
+    """
+    データセットの読み込み
+    # Arguments:
+        data_path           : 訓練データのパス (Numpy形式)
+        target_path         : 教師データのパス (Numpy形式)
+        target_label_path   : データセットのラベル (pickle形式)
+    # Returns:
+        dataset             : Bunchオブジェクト
+    ex)
+        dataset = load(data_path, target_path, target_label_path)
+        data = dataset.data
+        target = dataset.target
+        target_label = dataset.target_label
+    """
+    # 各種データの読み込み
+    data = np.load(data_path)
+    target = np.load(target_path)
+    target_label = load_binary_file(target_label_path)
+
+    dataset = Bunch(data=data, target=target,
+                    target_label=target_label)
+
+    return dataset
