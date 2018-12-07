7d77f1e72168dfaff08f3a75306284cfc9c21102
pphos <coc4h8conhc6h12nh@gmail.com>
2018-12-07 16:17:29 +0900

    add plot_confusion_matrix function

--- src/modules/utils/plot.py
+++ src/modules/utils/plot.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import itertools
 import numpy as np
 import matplotlib as mpl
 mpl.use('Agg')
@@ -43,4 +44,55 @@ def plot_loss_acc_history(history, save_dir, save_name,
     # グラフの保存
     figure_path = determine_save_name_path(save_dir, save_name, save_prefix)
     fig.savefig(figure_path)
+    plt.close()
+
+
+def plot_confusion_matrix(cm, target_names, save_dir, save_name,
+                          save_prefix=None, title=None,
+                          normalize=False, cmap=plt.cm.Blues):
+    """
+    sklearn confusion matrix example
+    URL :     URL:    https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+
+    Confusion Matrixの描画
+    # Arguments:
+        cm              : confusion matrix (Numpy配列)
+        target_names    : ラベル名のリスト
+        save_dir        : グラフを保存するディレクトリへのパス
+        save_name       : グラフの保存ファイル名
+        save_prefix     : 保存ファイル名に付与するプレフィックス
+        title           : グラフに付与するタイトル
+        normalize       : Confusion Matrixを正規化するかのフラグ (Bool)
+        cmap            : グラフのカラーマップ
+            ex) cmap=plt.cm.Blues
+    """
+    plt.figure(figsize=(16, 9))
+    plt.rcParams['font.size'] = 24
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+
+    if title is not None:
+        plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(target_names))
+    plt.xticks(tick_marks, target_names, rotation=45)
+    plt.yticks(tick_marks, target_names)
+
+    fmt = '.2f' if normalize else 'd'
+    threshold = cm.max() / 2.0
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment='center',
+                 color='white' if cm[i, j] > threshold else 'black')
+
+    plt.gcf().set_tight_layout(True)
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+    figure_path = determine_save_name_path(save_dir, save_name, save_prefix)
+
+    plt.savefig(figure_path)
     plt.close()
